1df1dcbc0881188307ed1f0f928be2fad0d8c6a0
RGUIG <ahmed.rguig@capgemini.com>
2019-04-11 15:51:00 +0200

    MessageService

--- message.service.ts
+++ message.service.ts
@@ -4,12 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
-message: string[]= [];
+messages: string[]= [];
   add(message : string){
-	  this.message.push(message);
+	  this.messages.push(message);
 }
 clear(){
-	this.message = [] ;
+	this.messages = [];
 }
 }
 
